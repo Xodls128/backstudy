fec0a1747ef4c363c1360249e1dc50cad7f7bc3e
xodls <pider6@naver.com>
2025-07-08 16:49:36 +0900

    FEATURE: 회원가입 url 구현 (backend/users/urls.py)

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import UserSignupView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('signup/', UserSignupView.as_view, name='user-signup'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #로그인 시 사용하는 API : (접근토큰과 리프레쉬) 토큰 발급
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), #토큰 새로 발급해주는 API : 리프레쉬 토큰을 바탕으로 접근 토큰 새로 발급해 줌
 ]
