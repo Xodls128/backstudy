84e6216f4311bfadf0d2b18eee298d7bea811189
xodls <pider6@naver.com>
2025-07-08 16:45:56 +0900

    FEATURE: 회원가입 뷰 구현 (backend/users/views.py)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSignupSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserSignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSignupSerializer
+    
