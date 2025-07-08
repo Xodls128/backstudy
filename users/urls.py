ff4ead6362fd59cb277447c2ad5a9b93988c6c0e
xodls <pider6@naver.com>
2025-07-08 15:51:17 +0900

    jwt 주석설명

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #로그인 시 사용하는 API : (접근토큰과 리프레쉬) 토큰 발급
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), #토큰 새로 발급해주는 API : 리프레쉬 토큰을 바탕으로 접근 토큰 새로 발급해 줌
 ]
