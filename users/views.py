c84f59b407b065afc741a3578814efea0e742301
xodls <pider6@naver.com>
2025-07-09 17:24:19 +0900

    CORRECT: 회원가입 누구나 가능하게 퍼미션설정 (backend/users/views.py) , url오타 정정 (backend/users/urls.py) , url 이름 의미에 맞게 수정 (backend/config/urls.py) , CORS 추가 (pipenv , backend/config/settings.py)

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from .serializers import UserSignupSerializer
 from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 
 class UserSignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSignupSerializer
-    
+    permission_classes = [AllowAny] 
